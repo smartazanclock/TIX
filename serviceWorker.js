1910ea3f7e1d4c32bf92ddd4975058c593d16411
SmartAzanClock <smartazanclock@gmail.com>
2024-06-26 22:44:16 -0400

    icon + default mode

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,10 +1,11 @@
-chrome.runtime.onInstalled.addListener(() => init());  
-chrome.runtime.onStartup.addListener(() => run());   
+chrome.runtime.onInstalled.addListener(() => init());
+chrome.runtime.onStartup.addListener(() => run());
 chrome.alarms.onAlarm.addListener((alarm) => run());
 
 const init = () => {
     let msgs = {};
-    fetch('../_locales/en/messages.json').then((response)  => { response.json().then((data) => {
+    fetch('../_locales/en/messages.json').then((response) => {
+        response.json().then((data) => {
             Object.entries(data).forEach(([key, value]) => { msgs[key] = value.message });
             chrome.storage.local.set({ 'msgs': msgs }, () => {
                 run();
@@ -20,13 +21,13 @@ const init = () => {
 const run = () => {
     chrome.storage.local.get(['msgs'], (result) => {
         let dateOptions = { weekday: "long", year: "numeric", month: "long", day: "numeric" };
-        let iconPath = result.msgs.icon;
         let dt = new Date(new Date().toLocaleString("en-US", { timeZone: result.msgs.timeZone }));
         let ds = dt.toLocaleString("en-US", dateOptions);
         let ampm = "";
         let hours = dt.getHours();
-        if (result.msgs.mode == "12")
-        {
+        let hourMode = ' (' + result.msgs.mode + 'hr mode)';
+        if (result.msgs.mode == "12") {
+            hourMode = '';
             if (hours > 12)
                 ampm = "pm"
             else
@@ -34,8 +35,7 @@ const run = () => {
             hours = hours % 12 || 12;
         }
         chrome.action.setBadgeText({ 'text': hours + ':' + zeroPad(dt.getMinutes()) });
-        chrome.action.setIcon({ 'path': iconPath });
-        chrome.action.setTitle({ 'title': ds + ' ' + hours + ':' + zeroPad(dt.getMinutes()) + ampm + ' (' + result.msgs.mode + 'hr mode)'});
+        chrome.action.setTitle({ 'title': ds + ' ' + hours + ':' + zeroPad(dt.getMinutes()) + ampm + hourMode });
         chrome.action.setBadgeBackgroundColor({ 'color': '#2E3338' });
     });
 };
@@ -46,7 +46,7 @@ chrome.action.onClicked.addListener((tab) => {
     chrome.storage.local.get(['msgs'], (result) => {
         result.msgs.mode = (result.msgs.mode == "12") ? 24 : 12;
         chrome.storage.local.set({ 'msgs': result.msgs }, () => {
-            chrome.action.setBadgeText({ 'text': result.msgs.mode+'hr' });
+            chrome.action.setBadgeText({ 'text': result.msgs.mode + 'hr' });
             setTimeout(run, 1000);
         });
 
